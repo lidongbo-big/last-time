0e381cfd65f7c9c302013914f45014a4b09de89f
lidongbo <1144086550@qq.com>
2020-08-24 15:24:41 +0800

    素材管理功能

--- src/api/image.js
+++ src/api/image.js
@@ -1,9 +1,18 @@
 import request from '@/utils/request'
 
+// 上传图片
 export const uploadImage = data => {
   return request({
     method: 'POST',
     url: '/mp/v1_0/user/images',
     data
+  })
+}
+// 加载图片
+export const getImage = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
   })
 }
--- src/views/images/index.vue
+++ src/views/images/index.vue
@@ -7,76 +7,86 @@
           <el-breadcrumb-item>素材管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
-      <div>
-        <el-radio-group v-model="radio1">
-          <el-radio-button label="全部"></el-radio-button>
-          <el-radio-button label="收藏"></el-radio-button>
+      <div class="action-head">
+        <el-radio-group @change="onCollcetChange" v-model="collect">
+          <el-radio-button :label="false">全部</el-radio-button>
+          <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
+        <el-button @click="dialogUpdateVisible=true" type='success'>上传素材</el-button>
       </div>
       <el-row :gutter="20">
-        <el-col :xs='12' :sm='6' :md='6' :lg='4'>
+        <el-col v-for="(image, index) in images" :key="index" :xs='12' :sm='6' :md='6' :lg='4'>
           <el-image
           style="width: 100px; height: 100px"
-          :src="url"
-          :fit="fit">
-          </el-image>
-        </el-col>
-        <el-col :xs='12' :sm='6' :md='6' :lg='4'>
-          <el-image
-          style="width: 100px; height: 100px"
-          :src="url"
-          :fit="fit">
-          </el-image>
-        </el-col>
-        <el-col :xs='12' :sm='6' :md='6' :lg='4'>
-         <el-image
-          style="width: 100px; height: 100px"
-          :src="url"
-          :fit="fit">
-          </el-image>
-        </el-col>
-        <el-col :xs='12' :sm='6' :md='6' :lg='4'>
-          <el-image
-          style="width: 100px; height: 100px"
-          :src="url"
-          :fit="fit">
-          </el-image>
-        </el-col>
-        <el-col :xs='12' :sm='6' :md='6' :lg='4'>
-          <el-image
-          style="width: 100px; height: 100px"
-          :src="url"
-          :fit="fit">
-          </el-image>
-        </el-col>
-        <el-col :xs='12' :sm='6' :md='6' :lg='4'>
-          <el-image
-          style="width: 100px; height: 100px"
-          :src="url"
-          :fit="fit">
+          :src="image.url"
+          fit="cover">
           </el-image>
         </el-col>
       </el-row>
     </el-card>
+    <el-dialog :append-to-body='true' title="上传素材" :visible.sync="dialogUpdateVisible">
+      <el-upload
+        class="upload-demo"
+        drag
+        :headers='uploadHeaders'
+        :on-success='onUploadSuccess'
+        name='image'
+        action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+        :show-file-list='false'
+        multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { getImage } from '@/api/image'
 export default {
   name: 'imagesIndex',
   components: {},
   props: {},
   data () {
+    const user = JSON.parse(window.localStorage.getItem('user'))
     return {
-      radio1: '全部'
+      collect: 'false',
+      images: [],
+      dialogUpdateVisible: false,
+      uploadHeaders: {
+        Authorization: `Bearer ${user.token}`
+      }
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadImages()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    loadImages (flag = false) {
+      getImage().then(res => {
+        this.images = res.data.data.results
+      })
+    },
+    onCollcetChange (value) {
+      console.log(value)
+      this.loadImages(value)
+    },
+    onUploadSuccess () {
+      this.loadImages(false)
+      this.dialogUpdateVisible = false
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.action-head {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 0;
+}
+</style>
