1f96d2fe0a9c9289a960fcc5f6e962cec5085baf
lidongbo <1144086550@qq.com>
2020-08-28 19:21:32 +0800

    最终提交

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -114,7 +114,7 @@ export default {
           {
             validator (rule, value, callback) {
               if (value === '<p></p>') {
-                callback(new Error('请输入shaye'))
+                callback(new Error('请输入标题'))
               } else {
                 callback()
               }
